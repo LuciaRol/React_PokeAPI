cbfa387712a060052a449ac549c9515f7b2c8d6c
LuciaRol <luciarol87@gmail.com>
2024-02-09 10:39:09 +0100

    conexion a firebase y fichero firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ-YAgKrHwEawv_tpE4tqjxR-gHOiil8A",
+  authDomain: "pokedex-79ee5.firebaseapp.com",
+  projectId: "pokedex-79ee5",
+  storageBucket: "pokedex-79ee5.appspot.com",
+  messagingSenderId: "138206337561",
+  appId: "1:138206337561:web:97bdcb7351c70be1de2888"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
