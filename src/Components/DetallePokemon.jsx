1d88ffb0d18899cf12ca4cf6e2a857d18f78eb63
LuciaRol <luciarol87@gmail.com>
2024-02-09 19:43:16 +0100

    añadidos stats a los detalles

--- src/Components/DetallePokemon.jsx
+++ src/Components/DetallePokemon.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
-// traducción de los tipos
+// Traducción de los tipos
 const tiposTraducidos = {
     normal: 'Normal',
     fighting: 'Lucha',
@@ -23,7 +23,7 @@ const tiposTraducidos = {
     fairy: 'Hada'
 };
 
-// traducción de los grupo huevo
+// Traducción de los grupos huevo
 const gruposHuevoTraducidos = {
     monster: 'Monstruo',
     water1: 'Agua 1',
@@ -43,51 +43,86 @@ const gruposHuevoTraducidos = {
     undiscovered: 'No descubierto'
 };
 
+// Traducción de los hábitats
+const habitatsTraducidos = {
+    cave: 'Cueva',
+    forest: 'Bosque',
+    grassland: 'Pradera',
+    mountain: 'Montaña',
+    rare: 'Raro',
+    roughTerrain: 'Terreno abrupto',
+    sea: 'Mar',
+    urban: 'Urbano',
+    watersEdge: 'Orilla del agua',
+    desert: 'Desierto',
+    forestDungeon: 'Bosque (Mazmorra)',
+    mountainDungeon: 'Montaña (Mazmorra)',
+};
+
+// Traducción de los stats
+const statsTraducidos = {
+    hp: 'PS',
+    attack: 'Ataque',
+    defense: 'Defensa',
+    specialAttack: 'AtaqueEspecial',
+    specialDefense: 'DefensaEspecial',
+    speed: 'Velocidad'
+};
+
 export default function DetallePokemon() {
-  const [detallePokemon, setDetallePokemon] = useState(null);
-  const [grupoHuevo, setGrupoHuevo] = useState('');
-  const { id } = useParams();
+    const [detallePokemon, setDetallePokemon] = useState(null);
+    const [grupoHuevo, setGrupoHuevo] = useState('');
+    const [habitat, setHabitat] = useState('');
+    const { id } = useParams();
 
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then(response => response.json())
-      .then(datosPokemon => {
-        setDetallePokemon(datosPokemon);
-        console.log(datosPokemon);
-      });
-  }, [id]);
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(response => response.json())
+            .then(datosPokemon => {
+                setDetallePokemon(datosPokemon);
+            });
+    }, [id]);
 
-  useEffect(() => {
-    if (detallePokemon) {
-      fetch(detallePokemon.species.url)
-        .then(response => response.json())
-        .then(datosPokemon => {
-          const huevo = datosPokemon.egg_groups.map(group => gruposHuevoTraducidos[group.name]).join(', ');
-          setGrupoHuevo(huevo);
-        });
-    }
-  }, [detallePokemon]);
+    useEffect(() => {
+        if (detallePokemon) {
+            fetch(detallePokemon.species.url)
+                .then(response => response.json())
+                .then(datosPokemon => {
+                    const huevo = datosPokemon.egg_groups.map(group => gruposHuevoTraducidos[group.name]).join(', ');
+                    const habitat = datosPokemon.habitat ? habitatsTraducidos[datosPokemon.habitat.name] : 'Desconocido';
+                    setGrupoHuevo(huevo);
+                    setHabitat(habitat);
+                });
+        }
+    }, [detallePokemon]);
 
-  return (
-    <>
-      <section>
-        <h1 className="title">DETALLE DEL POKÉMON</h1>
-        <div className="btn-group">
-          <button className="btn"><Link to="/listapokemon"><span className="nav-link">Volver</span></Link></button>
-          <button className="btn"><Link to="/jugar"><span className="nav-link">Ir a jugar</span></Link></button>
-        </div>
-        {detallePokemon && (
-          <div>
-            <img src={detallePokemon.sprites.front_default} alt={detallePokemon.name} />
-            <p>Número: {detallePokemon.id}</p>
-            <p>Tipo: {detallePokemon.types.map(type => tiposTraducidos[type.type.name]).join(', ')}</p>
-            <p>Peso: {detallePokemon.weight / 10} kg</p>
-            <p>Altura: {detallePokemon.height / 10} m</p>
-            <p>Grupo Huevo: {grupoHuevo}</p>
-          </div>
-        )}
-        {!detallePokemon && <p>Cargando...</p>}
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section>
+                <h1 className="title">DETALLE DEL POKÉMON</h1>
+                <div className="btn-group">
+                    <button className="btn"><Link to="/listapokemon"><span className="nav-link">Volver</span></Link></button>
+                    <button className="btn"><Link to="/jugar"><span className="nav-link">Ir a jugar</span></Link></button>
+                </div>
+                {detallePokemon && (
+                    <div>
+                        <img src={detallePokemon.sprites.front_default} alt={detallePokemon.name} />
+                        <p>Número: {detallePokemon.id}</p>
+                        <p>Tipo: {detallePokemon.types.map(type => tiposTraducidos[type.type.name]).join(', ')}</p>
+                        <p>Peso: {detallePokemon.weight / 10} kg</p>
+                        <p>Altura: {detallePokemon.height / 10} m</p>
+                        <p>Grupo Huevo: {grupoHuevo}</p>
+                        <p>Hábitat: {habitat}</p>
+                        <p>Stats:</p>
+                        <ul>
+                            {detallePokemon.stats.map(stat => (
+                                <li key={stat.stat.name}>{statsTraducidos[stat.stat.name]}: {stat.base_stat}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                {!detallePokemon && <p>Cargando...</p>}
+            </section>
+        </>
+    );
 }
