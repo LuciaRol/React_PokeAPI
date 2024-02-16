46451e8674f24376adf0d0743dd75dca49ac48c7
LuciaRol <luciarol87@gmail.com>
2024-02-16 19:13:15 +0100

    estilos de la pokedex

--- src/Components/ListaPokemon.jsx
+++ src/Components/ListaPokemon.jsx
@@ -6,56 +6,59 @@ import DetallePokemon from './DetallePokemon';
 
 function ListaPokemon() {
     const [listaPokemon, setListaPokemon] = useState([]);
+    const [cargando, setCargando] = useState(false); 
 
     useEffect(() => {
+        setCargando(true); 
         fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=8')
             .then(response => response.json())
             .then(datosPokemon => {
                 setListaPokemon(datosPokemon.results);
+                setCargando(false); 
             });
     }, []);
 
     const cargarMas = () => {
+        setCargando(true); 
         fetch('https://pokeapi.co/api/v2/pokemon/?offset=' + listaPokemon.length + '&limit=8')
             .then(response => response.json())
             .then(datosPokemon => {
                 setListaPokemon(prevListaPokemon => [...prevListaPokemon, ...datosPokemon.results]);
+                setCargando(false); 
             });
     };
 
     const handleSaberMas = pokemonUrl => {
         console.log('Saber más sobre:', pokemonUrl);
-        // Aquí puedes hacer lo que necesites cuando se selecciona un Pokémon
     };
 
     let lista = listaPokemon.map((pokemon, index) => (
-        <div key={pokemon.name} class="pokemon-card">
+        <div key={pokemon.name} className="pokemon-card">
             <img
-                class="img-lista"
+                className="img-lista"
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png`} 
                 alt={pokemon.name}
             />
-            <div class="pokemon-info">
-                <span>{pokemon.name}</span>
+            <div className="pokemon-info">
+                <span className="pokemon-name">{pokemon.name}</span>
                 <Link to={`/detalle/${pokemon.url.split('/')[6]}`}>
-                    <span class="nav-link">Saber más</span>
+                    <span className="nav-link">Saber más</span>
                 </Link>
             </div>
         </div>
     ));
 
     return (
-        <>
-        <h1>Componente para lista Pokémon</h1>
-        <div className="card">
-            <div className="pokemon-container">
-                {lista}
+        <div className="container">
+            <h1>Pokédex</h1>
+            <div className="card">
+                <div className="pokemon-container">
+                    {lista}
+                </div>
+                {cargando && <p>Cargando...</p>}
+                {!cargando && <button className="btn" onClick={cargarMas}>Cargar más</button>} 
             </div>
-            <button class="btn" onClick={cargarMas}>Cargar más</button>
         </div>
-            
-            
-        </>
     );
 }
 
